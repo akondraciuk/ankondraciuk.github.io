5bba56369a6a0e0546e250085b76bb68e22ca87b
akondraciuk <annakondraciuk123@me.com>
2015-12-11 00:18:22 -0500

    personal blog yo

---
+++ personal/js/about.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#toggle').click(function () {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+
+        $('h1').fadeToggle(0.35);
+
+
+    });
+    jquery(".container").fitText();
+
+});
